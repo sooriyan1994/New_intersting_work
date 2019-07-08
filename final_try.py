b6572dab4c04e246a805010fe4c6c7a8f748433b
sooriyan1994 <45000714+sooriyan1994@users.noreply.github.com>
2019-07-08 18:57:12 +0530

    Add files via upload

--- final_try.py
+++ final_try.py
@@ -23,32 +23,35 @@ V_f = 0.5
 n = int((V_f * A)/A_f)
 
 # compute grid shape based on number of points
-num_y = int((np.int(np.sqrt(n)) + 1)/2)
-num_x = int(np.int(n / (2 * num_y)) + 1)
+num_y = np.int(np.sqrt(n/2) + 1)
+num_x = np.int(n / (2 * num_y) + 1)
 
 dist_border = 0.1 # distance b/w the edge and the edge of fiber
 
 # create regularly spaced neurons
 x_s = np.linspace(dist_border + d/2, a-(dist_border + d/2), num_x, dtype=np.float)
 y_s = np.linspace(dist_border + d/2, a-(dist_border + d/2), num_y, dtype=np.float)
-coords_s = np.stack(np.meshgrid(x_s, y_s), -1).reshape(-1,2)
+#coords = np.stack(np.meshgrid(x_s, y_s), -1).reshape(-1,2) #-1 simply means that it is an unknown dimension
 
 #creating hexagonal packing
-x_t = np.linspace(dist_border + dis/2 + d, a-(dist_border + d + dis/2), num_x-1, dtype=np.float)
-y_t = np.linspace(dist_border + dis/2 + d, a-(dist_border + d + dis/2), num_y-1, dtype=np.float)
-coords_t = np.stack(np.meshgrid(x_t, y_t), -1).reshape(-1,2) #-1 simply means that it is an unknown dimension
-coords = np.append(coords_s, coords_t).reshape(-1,2)
+x_t = np.arange(x_s[0] + (x_s[1]- x_s[0])/2 , a-(dist_border + d/2), x_s[1]-x_s[0], dtype=np.float)
+y_t = np.arange(y_s[0] + (y_s[1]- y_s[0])/2, a-(dist_border + d/2), y_s[1]-y_s[0], dtype=np.float)
+
+coords = np.append(np.stack(np.meshgrid(x_s, y_s), -1), np.stack(np.meshgrid(x_t, y_t), -1)).reshape(-1,2)
 
 # compute spacing
-##init_dist = np.min((x[1]-x[0], y[1]-y[0]))
-##min_dist = init_dist * (1 - sensitivity)
-##
-##assert init_dist >= min_dist
-##print(min_dist)
-##
-### perturb points
-##max_movement = (init_dist - min_dist)/2
-##noise = np.random.uniform(
+
+init_dist = np.sqrt((x_t[0]-x_s[0])**2+(y_t[0]-y_s[1])**2)
+
+dis = 0.1 #Distance betweeen two fibers
+min_dist = d + dis # Minimum Distance between two fiber centers
+
+assert init_dist >= min_dist
+
+# perturb points
+max_movement = (init_dist - min_dist)/2
+noise_x = np.random.uniform(-1,1, size = (len(coords),2)
+noise_y = 
 ##    low=-max_movement,
 ##    high=max_movement,
 ##    size=(len(coords), 2))
